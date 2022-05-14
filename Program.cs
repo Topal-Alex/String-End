f462030b34e1559dfe805a8e8de8c5469b7439f1
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-14 16:33:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace ConsoleApplication1
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            Solution("samurai", "ai");
+
+
+        } 
+        public static bool Solution(string str, string ending)
+        {
+            return str.EndsWith(ending);
+        }
+        
+    }
+}
